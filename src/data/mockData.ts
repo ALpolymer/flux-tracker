ee6a4b65bcf6f639dfa5cf643b271917e247ddb9
Alexandros Polymeros <alpolymer@gmail.com>
2025-12-02 14:48:00 +0200

    refactor(data): change mockUser object to mockUsers array of objects

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -4,54 +4,65 @@ import {getRandomDate} from "../utils/getRandomDate.ts";
 import {generateMockTransactions} from "../utils/generateMockTransactions.ts";
 
 
-export const mockUser:User = {
-    id: generateId(),
-    username: "demo user 1",
-    createdAt: new Date().toISOString(),
-    email: "demo@example.com",
-    password: "hashed_password_123",
-}
+export const mockUsers:User[] = [
+    {
+        id: generateId(),
+        username: "user1",
+        createdAt: new Date().toISOString(),
+        email: "user1@example.com",
+        password: "Password1!",
+
+    },
+    {
+        id: generateId(),
+        username: "user2",
+        createdAt: new Date().toISOString(),
+        email: "user2@example.com",
+        password: "Password2!",
+    }
+
+]
 
 export const mockCategories:Category[] = [
     {
         id: generateId(),
-        userId: mockUser.id,
+        userId: mockUsers[1].id,
         name:"Food",
         description: "Restaurants, groceries, etc",
         createdAt: getRandomDate(30),
     },
     {
         id: generateId(),
-        userId: mockUser.id,
+        userId: mockUsers[2].id,
         name:"Gas",
         createdAt: getRandomDate(30),
     },
     {   id: generateId(),
-        userId: mockUser.id,
+        userId: mockUsers[1].id,
         name:"Electricity",
         createdAt: getRandomDate(30),
     },
     {
         id: generateId(),
-        userId: mockUser.id,
+        userId: mockUsers[1].id,
         name:"Entertainment",
         createdAt: getRandomDate(30),
     },
     {
         id: generateId(),
-        userId: mockUser.id,
+        userId: mockUsers[0].id,
         name:"Clothing",
         createdAt: getRandomDate(30),
     },
     {
         id: generateId(),
-        userId: mockUser.id,
+        userId: mockUsers[1].id,
         name:"Loan",
         createdAt: getRandomDate(30),
     },
     {
         id: generateId(),
-        userId: mockUser.id,
+        userId: mockUsers[0].id,
         name:"Health",
         createdAt: getRandomDate(30),
     }
@@ -61,21 +72,21 @@ export const mockCategories:Category[] = [
 export const mockWallets:Wallet[] = [
     {
         id: generateId(),
-        userId: mockUser.id,
+        userId: mockUsers[1].id,
         name: "Alpha Bank",
         balance: 1000,
         createdAt: new Date().toISOString(),
     },
     {
         id: generateId(),
-        userId: mockUser.id,
+        userId: mockUsers[0].id,
         name: "Piraeus Bank",
         balance: 2000,
         createdAt: new Date().toISOString(),
     },
     {
         id: generateId(),
-        userId: mockUser.id,
+        userId: mockUsers[1].id,
         name: "Binance",
         balance: 2000,
         createdAt: new Date().toISOString(),
