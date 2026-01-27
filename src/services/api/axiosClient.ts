2daa042e5f2a6a527f726ef74b486361f94920f7
Alexandros Polymeros <alpolymer@gmail.com>
2026-01-27 21:29:34 +0200

    feat: add axiosClient.ts

---
+++ src/services/api/axiosClient.ts
@@ -0,0 +1,52 @@
+import axios from 'axios';
+import  {STORAGE_KEYS} from "../localStorage/types.ts";
+
+
+
+const axiosClient = axios.create({
+    baseURL: 'http://localhost:8080',
+    headers: {
+        "Content-Type": "application/json",
+    }
+})
+
+axiosClient.interceptors.request.use(
+    (config) => {
+        const isAuthEndpoint = config.url?.startsWith('/api/auth');
+
+        if(!isAuthEndpoint){
+            const stored = localStorage.getItem(STORAGE_KEYS.USER);
+
+            if(stored){
+                const authResponse = JSON.parse(stored);
+
+                config.headers.Authorization = `Bearer ${authResponse.token}`
+
+                console.log(' Token attached to request:', config.url);  // Debug
+
+            }
+        }
+
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+)
+
+axiosClient.interceptors.response.use(
+    (response) =>{
+        return response;
+    },
+    (error) => {
+        if(error.response?.status === 401){
+            console.log("Unauthorized");
+            localStorage.removeItem(STORAGE_KEYS.USER);
+            window.location.href = '/signin';
+        }
+
+        return Promise.reject(error);
+    }
+)
+
+export default axiosClient;
